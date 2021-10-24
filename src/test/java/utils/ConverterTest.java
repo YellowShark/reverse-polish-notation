f30a37dbbbf8d4203abbc22092348b9881ef8683
Daniil Dobrodeev <danil.dobrodeev@mail.ru>
2021-10-24 15:32:48 +0300

    wrote some tests

--- src/test/java/utils/ConverterTest.java
+++ src/test/java/utils/ConverterTest.java
@@ -20,6 +20,12 @@ public class ConverterTest {
     }
 
     @Test
+    public void test_precedence() {
+        var exp = "2^2+2*2";
+        assertEquals("2 2 ^ 2 2 * +", Converter.infixToPostfix(exp));
+    }
+
+    @Test
     public void test_complexExpression() {
         var exp = "((10*(6*((9+3)*11)))+17)+5";
         assertEquals("10 6 9 3 + 11 * * * 17 + 5 +", Converter.infixToPostfix(exp));
